1110c7e499fb4b6fb094f0618c35df8149d6d373
T4rm <contact@mechkenetarek.com>
2024-02-08 10:57:11 +0100

    Reapply "Add custom accordion and about page content"
    This reverts commit 9d7c3108fad72cb5acef3b568aa9645f1cfd0274.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import Arasaka3D from '../components/spline/logo/Arasaka3D';
+import Accordion from '../components/accordeon/Accordion';
+import gsap from "gsap";
+import { useGSAP } from '@gsap/react';
 
 function About() {
+
+    useGSAP(() => {
+        gsap.fromTo(".reveal-opacity", { opacity: 0 }, { opacity: 1, duration: 2, delay: 1 })
+    });
     return (
         <>
-            <h1>ABOUT ARASAKA</h1>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <div className="flex col center t-center" >
+                <Arasaka3D />
+                <div className='reveal-opacity'>
+                    <h1>ABOUT US - ARASAKA</h1>
+                    <br />
+                    <p>Arasaka is a Japanese multinational corporation with its headquarters located in Night City, Northern California. The company is one of the world's leading manufacturers of cybernetic implants, weapons, vehicles and robotics.</p>
+                    <br />
+                    <p>Founded in 1919 by Saburo Arasaka, the company is currently led by his grandson, Kei Arasaka, and is one of the most influential corporations in the world.</p>
+                    <br />
+                    <hr />
+                    <br />
+                    <h2>OUR BUSINESSES</h2>
+                    <Accordion />
+                </div>
+            </div>
         </>
     );
 }
